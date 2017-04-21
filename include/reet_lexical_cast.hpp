bc172f13742d22d6794fd79b131a3e63c57fd4df
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2017-04-21 23:26:56 +0300

    template specialization to/from basic_string

--- include/reet_lexical_cast.hpp
+++ include/reet_lexical_cast.hpp
@@ -8,30 +8,55 @@
 #ifndef INCLUDE_REET_LEXICAL_CAST_HPP_
 #define INCLUDE_REET_LEXICAL_CAST_HPP_
 
+#include <cstddef>
 #include <sstream>
 
-template <typename T, typename FromT> struct serializal {
-	static T cast(FromT const& from) {
-		std::ostringstream oss;
+namespace reet {
+
+template <typename T, typename FromT> struct impl {
+	static T lexical_cast(FromT const& from);
+};
+
+//from basic string
+template <typename T, typename FromT> struct impl<T, std::basic_string<FromT> > {
+	static T lexical_cast(std::basic_string<FromT> const& from) {
+		std::basic_istringstream<FromT> iss(from);
+		T t;
+		iss >> t;
+		return t;
+	}
+};
+
+//to basic sting
+template <typename T, typename FromT> struct impl<std::basic_string<T>, FromT> {
+	static std::basic_string<T> lexical_cast(FromT const& from) {
+		std::basic_ostringstream<T> oss;
 		oss << from;
-		return serializal<T, std::string>::cast(oss.str());
+		return oss.str();
 	}
 };
 
-template <typename T> struct serializal<T, std::string> {
-	static T cast(const std::string& str) {
-		std::istringstream iss;
-		iss.str(str);
-		T var;
-		iss >> var;
-		return var;
+//identity
+template <typename T> struct impl<T, T> {
+	static T lexical_cast(T const& t) {
+		return t;
 	}
 };
 
-template<> struct serializal<std::string, std::string> {
-	static std::string cast(const std::string& str) {
-		return str;
-	}
-};
+template <typename T, typename FromT> T lexical_cast(FromT const& from) {
+	return impl<T, FromT>::lexical_cast(from);
+}
+
+//from C string
+template <typename T, typename FromT> T lexical_cast(FromT const* from) {
+	return lexical_cast<T>(std::basic_string<FromT>(from));
+}
+
+//from range of characters
+template <typename T, typename FromT> T lexical_cast(FromT const* from, std::size_t n) {
+	return lexical_cast<T>(std::basic_string<FromT>(from, n));
+}
+
+} //namespace reet
 
 #endif /* INCLUDE_REET_LEXICAL_CAST_HPP_ */
